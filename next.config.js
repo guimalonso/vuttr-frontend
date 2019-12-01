360f79fede80f5fe003955476ce6b826d38966e8
guimalonso <guilherme.malonso@gmail.com>
2019-12-01 12:58:15 -0300

    Fixes for local and Heroku deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const withCSS = require('@zeit/next-css');
 module.exports = {
   ...withCSS(),
   env: {
-    REACT_APP_API_URL: process.env.REACT_APP_API_URL
+    REACT_APP_API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3000'
   }
 };
