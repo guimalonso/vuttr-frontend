885eb7413ba35812f5f1ac810d7b4630ce7b9e55
guimalonso <guilherme.malonso@gmail.com>
2019-12-01 09:52:45 -0300

    Preparations for Heroku deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000',
-});
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
+const api = axios.create({ baseURL });
 
 export default api;
